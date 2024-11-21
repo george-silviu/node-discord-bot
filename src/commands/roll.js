53eceb9ecce092d800dbf4ef8c5c8f8d951d7ee8
unknown <costache.georgesilviu@yahoo.ro>
2024-11-21 12:12:52 +0200

    implementing the roll command

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -1,7 +1,19 @@
 import { SlashCommandBuilder } from "discord.js";
 
+//generate random dice roll function
 function generateRandom(min = 1, max = 12) {
   return Math.floor(Math.random() * max) + min;
 }
 
-const data = new SlashCommandBuilder().setName("roll").setDescription();
+//define the command info
+const data = new SlashCommandBuilder()
+  .setName("roll")
+  .setDescription("Roll the dice!");
+
+//define the execution handler
+async function execute(interaction) {
+  const roll = generateRandom();
+  await interaction.reply(`🎲 Nice! You rolled ${roll}!`);
+}
+
+export { data, execute };
